e06a14be387f99e2cfdeb85c2075185f8e4526e8
DjNovovic <djordjijenovovic@gmail.com>
2020-09-02 17:50:45 +0200

    Initial Commit

---
+++ Weather01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather01
+//
+//  Created by Djordjije on 02/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather01/Weather01App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather01App.swift
+//  Weather01
+//
+//  Created by Djordjije on 02/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Weather01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
